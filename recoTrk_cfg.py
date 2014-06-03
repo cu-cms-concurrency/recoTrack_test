1e917628002631d6aaf44d6f9f39c0954a5e51de
Kevin McDermott <kpm82@cornell.edu>
2014-06-03 12:14:20 -0500

    Test track reco

---
+++ recoTrk_cfg.py
@@ -0,0 +1,80 @@
+import FWCore.ParameterSet.Config as cms
+
+#Works for CMSSW_7_1_0_pre8.  Used ttbar GEN-SIM-RECO samples.  
+
+process = cms.Process("RERECO")
+
+### standard MessageLoggerConfiguration
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+### source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+
+'/store/relval/CMSSW_7_1_0_pre8/RelValTTbar_13/GEN-SIM-RECO/PU25ns_PRE_LS171_V9-v1/00000/04EE8F23-9CE7-E311-AAC1-002590596498.root'
+
+#'/store/relval/CMSSW_7_1_0_pre8/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/PU25ns_PRE_LS171_V9-v1/00000/104FC845-61E7-E311-BBEA-0025905A60B2.root'
+
+#'/store/relval/CMSSW_7_1_0_pre8/RelValZTT_13/DQM/PU25ns_PRE_LS171_V9-v1/00000/105CF56E-80E7-E311-97FC-0030486792B4.root'
+
+    )
+)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+### conditions
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+# same as input file
+#process.GlobalTag.globaltag = 'PRE_ST62_V8::All'
+# or get it automatically
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:startup', '')
+
+### standard includes (can't live without)
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('Configuration.StandardSequences.Geometry_cff')
+process.load("Configuration.StandardSequences.RawToDigi_cff")
+process.load("Configuration.EventContent.EventContent_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+### define tracking sequences to run
+# need to recreate hits from clusters (which are stored in RECO)
+process.clustToHits = cms.Sequence(
+    process.siPixelRecHits*process.siStripMatchedRecHits
+)
+# this is the official tracking sequence
+process.tracking = cms.Sequence(
+    process.MeasurementTrackerEvent*
+    process.siPixelClusterShapeCache*
+    process.trackingGlobalReco
+)
+
+### define output EDMFile
+MyReRecoEventContent = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+    'drop *',
+    'keep *_siPixelClusters_*_*',
+    'keep *_siStripClusters_*_*',
+    'keep *_offlineBeamSpot_*_*',
+    'keep *_pixelVertices_*_*',
+    'keep *_*_*_RERECO'),
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize=cms.untracked.int32(5*1024*1024)
+)
+process.out = cms.OutputModule("PoolOutputModule",
+    MyReRecoEventContent,
+    fileName = cms.untracked.string('reco_trk.root')
+)
+
+### paths
+process.p = cms.Path(process.clustToHits * process.tracking)
+process.o = cms.EndPath(process.out)
+
+### sequence of paths to run
+process.schedule = cms.Schedule(process.p,process.o)
+
+### debug time and memory information (more at Validation/Performance/python//TimeMemory*.py)
+#process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+#process.SimpleMemoryCheck=cms.Service("SimpleMemoryCheck",
+#                                      ignoreTotal=cms.untracked.int32(1),
+#                                      oncePerEventMode=cms.untracked.bool(False))
